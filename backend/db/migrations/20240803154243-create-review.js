c464008c6b09c828166097f4e619d69ca6835250
Ruben <rubenngalvann99@gmail.com>
2024-08-03 12:13:52 -0500

    added model, migration, and seeds and tested db-reset

---
+++ backend/db/migrations/20240803154243-create-review.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Spots'
+        }
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users'
+        }
+      },
+      review: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      stars: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    }, options);
+    await queryInterface.addIndex('Reviews', ['userId', 'spotId'],
+        {
+          ...options,
+          unique: true
+        }
+    )
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Reviews";
+    await queryInterface.removeIndex("Reviews", ['userId', 'spotId'], options)
+    await queryInterface.dropTable(options);
+  }
+};
---
+++ backend/db/seeders/20240803160303-demo-review.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { Review } = require('../models')
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await Review.bulkCreate([
+    {
+      spotId: 1,
+      userId: 3,
+      review: 'Amazing place! Really enjoyed my stay.',
+      stars: 5
+    },
+    {
+      spotId: 2,
+      userId: 3,
+      review: 'The location was perfect, but the service could be better.',
+      stars: 3
+    },
+    {
+      spotId: 3,
+      userId: 1,
+      review: 'Had a fantastic experience. Will visit again.',
+      stars: 4
+    },
+    {
+      spotId: 3,
+      userId: 2,
+      review: 'Terrible place, do not rent.',
+      stars: 1
+    }
+   ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Reviews';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      userId: { [Op.in]: [1, 2, 3]}
+    }, {})
+  }
+};
