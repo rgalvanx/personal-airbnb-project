8dbfbdfb977166ccb33cacab49eebf9515ab5916
chris <chris.hubbard.dev@gmail.com>
2024-08-07 11:09:28 -0700

    debugging formattter

--- backend/app.js
+++ backend/app.js
@@ -74,8 +74,10 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
+
   if(isProduction) {
     return res.json({
+      justChecking: 'is this working',
       message: err.message,
       errors: err.errors
     })
