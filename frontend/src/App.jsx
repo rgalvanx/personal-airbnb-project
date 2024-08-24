911b4e16529d96d094650d7572888a685896e964
Ruben G <159964698+rgalvanx@users.noreply.github.com>
2024-08-24 16:19:44 -0500

    implemented navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-// frontend/src/App.jsx
-
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';
-import * as sessionActions from './store/session';
 import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+import Navigation from './components/Navigation/Navigation';
+import * as sessionActions from './store/session';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -19,6 +18,7 @@ function Layout() {
 
   return (
     <>
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,12 @@
+import { FaDog } from 'react-icons/fa6'
+
+const Dog = () => {
+    return (
+        <div style={{ color: "black", fontSize: "100px" }}>
+            <FaDog />
+        </div>
+    );
+};
+
+
+export default Dog;
