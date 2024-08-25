d4152ce688ca6e48b5556ffc771e0ca0675c3f8d
Ruben G <159964698+rgalvanx@users.noreply.github.com>
2024-08-25 11:17:33 -0500

    finished both modals

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage/SignupFormPage';
 import Navigation from './components/Navigation/Navigation';
 import * as sessionActions from './store/session';
+import LoginFormModal from './components/LoginFormModal/LoginFormModal';
+import SignupFormModal from './components/SignupFormModal/SignupFormModal';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -31,14 +31,6 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <h1>Welcome!</h1>
-      },
-      {
-        path: '/login',
-        element: <LoginFormPage />
-      },
-      {
-        path: '/signup',
-        element: <SignupFormPage />
       }
     ]
   }
